18356979f708fa6e20263f4e2945e519495bc59a
juanmasisti <sistijuanmanuel@gmail.com>
2023-05-16 02:30:58 -0300

    Intento firebase 01

--- src/main/java/com/juanma/portlofio/controller/CImagen.java
+++ src/main/java/com/juanma/portlofio/controller/CImagen.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 
     @RestController
     @RequestMapping("/imagen")
-    @CrossOrigin(origins = {"http://localhost:4200/"})
+    @CrossOrigin(origins = {"https://juanma-c04c8.web.app/"})
     
 public class CImagen {
     @Autowired
